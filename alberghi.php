e8bd03d0d385ab8f7deb2f7ca3d7500ff6ccdc61
PasqualeIannace <pasquale.iannace@gmail.com>
2023-12-18 23:38:58 +0100

    MILESTONE 1

--- alberghi.php
+++ alberghi.php
@@ -4,9 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
 <?php
+
+    $scope = 'scope="col"';
+    $count = 1;
 
     $hotels = [
 
@@ -61,5 +65,30 @@
     ?>
     </h3>
     <br>
+
+
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Nome</th>
+      <th scope="col">Distanza dal centro</th>
+      <th scope="col">Voto</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach ($hotels as $hotel) {
+        echo "<tr>";
+        echo "<th $scope> $count </th>";
+        echo "<td>" . $hotel["name"] . "</td>";
+        echo "<td>" . $hotel["distance_to_center"] . "</td>";
+        echo "<td>" . $hotel["vote"] . "</td>";
+        echo "</tr>";
+
+        $count++;
+    }
+    ?>
+  </tbody>
+</table>
 </body>
 </html>
